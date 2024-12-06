471db0aecf860b37b1a2d29d415dc6574a4f5501
Jakub Franek <jakub.franek@plus4u.net>
2024-12-06 16:22:55 +0100

    Day 06 puzzle 2: gets stuck.

--- src/day_06/puzzle.py
+++ src/day_06/puzzle.py
@@ -15,7 +15,6 @@ def puzzle_01() -> int:
     :return:
     """
 
-
     prototype_lab = PrototypeLab()
     prototype_lab.walk_guard()
     return prototype_lab.visited_positions
@@ -28,6 +27,22 @@ def puzzle_02() -> int:
     :return:
     """
 
-    return -1
+    solutions_count = 0
+    prototype_lab = PrototypeLab()
+    attempt_count = 0
+
+    for row in range(prototype_lab.map_height):
+        for column in range(prototype_lab.map_width):
+            if prototype_lab.is_free_space(row, column):
+                attempt_count += 1
+                prototype_lab.place_obstruction(row, column)
+                prototype_lab.walk_guard()
+
+                if prototype_lab.guard_stuck_in_loop:
+                    solutions_count += 1
+                    print(f"{attempt_count}/{prototype_lab.free_space_count}")
+                prototype_lab.reset_map()
+    return solutions_count
+    # return -1
 
 ################################################################################
